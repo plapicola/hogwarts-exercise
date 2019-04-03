938876d44889acf97ae02ca025c553efa1dd7d03
plapicola <p.lapicola@gmail.com>
2019-04-03 13:10:01 -0600

    Adds feature test for student spec

---
+++ spec/features/user_can_search_for_students_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Searching for students' do
+  context 'as a visitor' do
+    it 'I can search for students by house' do
+      visit root_path
+
+      select 'Slytherin', from: :search
+
+      click_on 'Get Students'
+
+      expect(current_path).to eq(search_path)
+
+      expect(page).to have_content("22 Students")
+
+      students = page.find_all('.student')
+
+      expect(students.length).to eq(22)
+
+      students.each do |student|
+        within student do
+          expect(page).to have_content("ID:")
+          expect(page).to have_content("Name:")
+        end
+      end
+    end
+  end
+end
