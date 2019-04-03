42c8564d76084dcdd146d75e4ff43eb4646b8101
plapicola <p.lapicola@gmail.com>
2019-04-03 14:19:03 -0600

    Updates service to keep common uri patterns in conn

--- app/services/hogwarts_service.rb
+++ app/services/hogwarts_service.rb
@@ -1,6 +1,6 @@
 class HogwartsService
   def students_for_house(house_id)
-    get_json("/api/v1/house/#{house_id}")
+    get_json("house/#{house_id}")
   end
 
   private
@@ -10,7 +10,7 @@ class HogwartsService
   end
 
   def conn
-    Faraday.new(url: 'http://hogwarts-as-a-service.herokuapp.com/') do |faraday|
+    Faraday.new(url: 'http://hogwarts-as-a-service.herokuapp.com/api/v1/') do |faraday|
       faraday.headers["x-api-key"] = 'oPGaRUMm7Cpisj6fosH8VhIgjnxx8zh1'
       faraday.adapter Faraday.default_adapter
     end
